a154c80c54d378da3d8d47509c446d02909f4619
duytnmse <duytnmse173241@fpt.edu.vn>
2023-10-16 15:11:14 +0700

    add process get form

--- SWP-BirdBreedingManagement/bird-new-version/app/(main)/process/page.tsx
+++ SWP-BirdBreedingManagement/bird-new-version/app/(main)/process/page.tsx
@@ -1,46 +1,61 @@
+"use client";
 import BreadScrum from '@/components/BreadScrum';
 import ProcessTable from '@/components/Table/ProcessTable';
+import ProcessClient from '@/components/Table/ProcessTable/ProcessClient';
+import { ProcessColumn } from '@/components/Table/ProcessTable/column';
+import useProcesses from '@/hooks/useProcess';
 
 import React from 'react'
 
-const page = () => {
-    return (
-      <>
-        <div id="main-wrapper" className="show">
-          <div className="content-body">
-            <div className="warper container-fluid">
-              <div className="all-patients main_container">
+const ProcessPage = () => {
+  const { processes } = useProcesses();
+  const formatProcesses: ProcessColumn[] = processes.map((process) => ({
+    id: process.id,
+    mother_id: process.mother_id,
+    father_id: process.father_id,
+    cage_id: process.cage_id,
+    bird_type: process.bird_type
+  }));
+
+  return (
+    <>
+      <div id="main-wrapper" className="show">
+        <div className="content-body">
+          <div className="warper container-fluid">
+            <div className="all-patients main_container">
               <BreadScrum title='Danh Sách Quá Trình' subRouteTitle='process' subTitle1='Danh Sách Quá Trình' />
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="card">
-                      <div className="card-header fix-card">
-                        <div className="row">
-                          <div className="col-8">
-                            <h4 className="card-title">Tất Cả Quá Trình </h4>
-                          </div>
-                          <div className="col-4 float-end">
-                            <a
-                              href="/cr-process"
-                              className="btn btn-primary float-end"
-                            >
-                              Thêm Quá Trình
-                            </a>
-                          </div>
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="card">
+                    <div className="card-header fix-card">
+                      <div className="row">
+                        <div className="col-8">
+                          <h4 className="card-title">Tất Cả Quá Trình </h4>
+                        </div>
+                        <div className="col-4 float-end">
+                          <a
+                            href="/cr-process"
+                            className="btn btn-primary float-end"
+                          >
+                            Thêm Quá Trình
+                          </a>
                         </div>
                       </div>
-                      <div className="container"> <ProcessTable/></div>
-                      
                     </div>
+                    <div className="container">
+                      {/* <ProcessTable /> */}
+                      <ProcessClient data={formatProcesses} />
+                    </div>
+
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
-  
-  export default page;
-  
+      </div>
+    </>
+  );
+};
+
+export default ProcessPage;
---
+++ SWP-BirdBreedingManagement/bird-new-version/hooks/useProcess.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+type ProcessProps = {
+    id: string,
+    mother_id: string,
+    father_id: string,
+    cage_id: string,
+    bird_type: string
+}
+
+const useProcesses = () => {
+
+    const [processes, setProcesses] = useState<ProcessProps[] | []>([]);
+    const [loading, setLoading] = useState(false)
+
+
+    useEffect(() => {
+        // CALL API (GET)
+
+        // Hàm fetch process
+        const fetchProcesses = async () => {
+            try {
+                const res = await axios.get(" http://localhost:3001/processes")
+
+                setProcesses(res.data)
+                setLoading(true)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+
+        //Gọi lại hàm để chạy
+        fetchProcesses();
+    }, [])
+
+    return { processes, loading }
+}
+
+export default useProcesses
