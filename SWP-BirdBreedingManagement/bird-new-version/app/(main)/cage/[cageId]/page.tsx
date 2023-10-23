a642dc907dd92dc1b48525914d14904e0146e687
duytnmse <duytnmse173241@fpt.edu.vn>
2023-10-23 19:10:08 +0700

    edit UI cage diagram

--- SWP-BirdBreedingManagement/bird-new-version/app/(main)/cage/[cageId]/page.tsx
+++ SWP-BirdBreedingManagement/bird-new-version/app/(main)/cage/[cageId]/page.tsx
@@ -48,14 +48,14 @@ const CageIdPage = () => {
             subTitle1="Tất Cả Lồng"
             subTitle2="Thông Tin Lồng"
           />
-          {/* {FilterCageByRole?.map((item) => (
+          {FilterCageByRole?.map((item) => (
             <BirdCard
               birdRole={item.reproductionRole}
               birdId={item.bird?.birdId}
               image={item.bird?.image}
               birdType={item.bird?.birdType?.name}
             />
-          ))} */}
+          ))}
 
           <div className="row">
             <div className="col-lg-12">
@@ -72,7 +72,7 @@ const CageIdPage = () => {
                       aria-controls="home"
                       aria-selected="true"
                     >
-                      Thông Tin Quá Trình
+                      Nhân viên phụ trách
                     </button>
                   </li>
                   <li className="nav-item col-md-4" role="presentation">
@@ -86,7 +86,7 @@ const CageIdPage = () => {
                       aria-controls="profile"
                       aria-selected="false"
                     >
-                      Chim non
+                      Thông tin quá trình
                     </button>
                   </li>
                   <li className="nav-item col-md-4" role="presentation">
@@ -110,6 +110,79 @@ const CageIdPage = () => {
                     id="home"
                     role="tabpanel"
                     aria-labelledby="home-tab"
+                  >
+                    <div className="card m-t-30">
+                      <div className="card-body ">
+                        {/* <p className=" flex justify-between">
+                          <div>
+                            <div className="flex justify-between items-center mb-2.5">
+                              <label className="basis-[100%]">
+                                NGÀY GHÉP CẶP :
+                              </label>
+                              <div className="grow pl-2.5 pb-1.5">
+                                11/10/2023
+                              </div>
+                            </div>
+
+                            <div className="flex justify-between items-center mb-2.5">
+                              <label className="basis-[100%]">
+                                NGÀY ĐẺ TRỨNG :
+                              </label>
+                              <div className="grow pl-2.5 pb-1.5">
+                                30/10/2023
+                              </div>
+                            </div>
+
+                            <div className="flex justify-between items-center mb-2.5">
+                              <label className="basis-[100%]">
+                                TỔNG SỐ TRỨNG:
+                              </label>
+                              <div className="grow pl-2.5 pb-1.5">10</div>
+                            </div>
+                          </div>
+
+                          <div>
+                            <div className="flex justify-between items-center mb-2.5">
+                              <label className="basis-[100%]">
+                                SỐ TRỨNG THẤT BẠI :
+                              </label>
+                              <div className="grow pl-2.5 pb-1.5">2</div>
+                            </div>
+
+                            <div className="flex justify-between items-center mb-2.5">
+                              <label className="basis-[100%]">MÃ LỒNG :</label>
+                              <div className="grow pl-2.5 pb-1.5">A001</div>
+                            </div>
+
+                            <div className="flex justify-between items-center mb-2.5">
+                              <label className="basis-[100%]">
+                                GIAI ĐOẠN :
+                              </label>
+                              <div className="grow pl-2.5 pb-2 basis-[100%]">
+                                ẤP TRỨNG
+                              </div>
+                            </div>
+                          </div>
+
+                          <br />
+                        </p> */}
+                        <div className="col-md-6 col-lg-4">
+                          {FindCageById?.user && (
+                            <SpStaff
+                              id={FindCageById.user.userId}
+                              name={FindCageById.user.fullName}
+                              role={FindCageById.user.role}
+                            />
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="tab-pane fade"
+                    id="profile"
+                    role="tabpanel"
+                    aria-labelledby="profile-tab"
                   >
                     <div className="card m-t-30">
                       <div className="card-body">
@@ -171,23 +244,6 @@ const CageIdPage = () => {
                   </div>
                   <div
                     className="tab-pane fade"
-                    id="profile"
-                    role="tabpanel"
-                    aria-labelledby="profile-tab"
-                  >
-                    <div className="card m-t-30">
-                      <div className="card-body">
-                        <div className="row">
-                          <BabyBirdCard />
-                          <BabyBirdCard />
-                          <BabyBirdCard />
-                          <BabyBirdCard />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="tab-pane fade"
                     id="contact"
                     role="tabpanel"
                     aria-labelledby="contact-tab"
@@ -222,7 +278,7 @@ const CageIdPage = () => {
             {/* <div className="col-md-6 col-lg-4">
               <NotificationCard />
             </div> */}
-            <div className="col-md-6 col-lg-4">
+            {/* <div className="col-md-6 col-lg-4">
               {FindCageById?.user && (
                 <SpStaff
                   id={FindCageById.user.userId}
@@ -230,7 +286,7 @@ const CageIdPage = () => {
                   role={FindCageById.user.role}
                 />
               )}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
--- SWP-BirdBreedingManagement/bird-new-version/components/CageDiagram/CageDiagramList.tsx
+++ SWP-BirdBreedingManagement/bird-new-version/components/CageDiagram/CageDiagramList.tsx
@@ -4,33 +4,36 @@ import React from "react";
 import CageDiagramItem from "./CageDiagramItem";
 import useCages from "@/hooks/useCage";
 const CageDiagramList = () => {
-    const { cages } = useCages();
-    const CageFilterLocationA = cages.filter((cage) => cage.location === "A");
-    return (
-        <div className="row">
-            <div className="col-lg-12">
-                <div className="widget-media list-doctors best-doctor">
-                    <div className="timeline row">
+  const { cages } = useCages();
+  const CageFilterLocationB = cages.filter(
+    (cage) => cage.location.charAt(0) === "B"
+  );
 
-                        {cages.map((item) => (
-                            <CageDiagramItem
-                                key={item.cageId}
-                                id={item.cageId}
-                            // name={bird.name}
-                            // specialty={bird.specialty}
-                            // imageSrc={bird.imageSrc}
-                            // starReview={item}
-                            // process={bird.process}
-                            />
+  console.log(CageFilterLocationB);
+  return (
+    <div className="row">
+      <div className="col-lg-12">
+        <div className="widget-media list-doctors best-doctor">
+          <div className="timeline row">
+            {cages.map((item) => (
+              <CageDiagramItem
+                key={item.cageId}
+                id={item.cageId}
+                crowIcons={item.quantity}
+                eggIcons={item.reproductionProcess?.totalEgg}
 
-                        ))}
-
-
-                    </div>
-                </div>
-            </div>
+                // name={bird.name}
+                // specialty={bird.specialty}
+                // imageSrc={bird.imageSrc}
+                // starReview={item}
+                // process={bird.process}
+              />
+            ))}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default CageDiagramList
+export default CageDiagramList;
